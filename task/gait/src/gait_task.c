4e20a91df48546ed77778d35f012f20c04c45289
SupermanOfHeiLinPu <2827138147@qq.com>
2020-04-17 11:51:38 +0800

    站立姿态控制由P，改为PID

--- task/gait/src/gait_task.c
+++ task/gait/src/gait_task.c
@@ -5,7 +5,40 @@ TaskHandle_t GaitTaskHandle;
 QueueHandle_t GaitQueue;
 
 /*
- 
+ *站立姿态PID结构体定义
+ */
+struct _stand_PID
+{
+    double Kp;
+    double Ki;
+    double Kd;
+    double pek;
+    double pek_1;
+    double rek;
+    double rek_1;
+    double perr_integral;
+    double rerr_integral;
+};
+/*
+ *初始化PID各项参数
+ */
+struct _stand_PID stand_PID = {
+    .Kp = 0.5,
+    .Ki = 0.00001,
+    .Kd = 1,
+    .pek_1 = 0,
+    .rek_1 = 0,
+    .perr_integral = 0,
+    .rerr_integral = 0};
+
+/*
+ *功能：走直线
+ *输入：
+    current_time：当前时间
+    T：周期
+    theta：驱动空间地址
+*输出：
+    theta：驱动空间
  */
 int go_straight(uint32_t current_time, double T, double theta[12])
 {
@@ -22,7 +55,13 @@ int go_straight(uint32_t current_time, double T, double theta[12])
 }
 
 /*
-
+*右转
+*输入：
+    current_time：当前时间
+    T：周期
+    theta：驱动空间地址
+*输出：
+    theta：驱动空间
 */
 int go_right(uint32_t current_time, double T, double theta[12])
 {
@@ -38,6 +77,15 @@ int go_right(uint32_t current_time, double T, double theta[12])
     return 0;
 }
 
+/*
+ *左转
+ *输入：
+    current_time：当前时间
+    T：周期
+    theta：驱动空间地址
+ *输出：
+    theta：驱动空间
+ */
 int go_left(uint32_t current_time, double T, double theta[12])
 {
     uint32_t deltaT;
@@ -52,6 +100,15 @@ int go_left(uint32_t current_time, double T, double theta[12])
     return 0;
 }
 
+/*
+*后退
+*输入：
+    current_time：当前时间
+    T：周期
+    theta：驱动空间地址
+*输出：
+    theta：驱动空间
+*/
 int go_back(uint32_t current_time, double T, double theta[12])
 {
     uint32_t deltaT;
@@ -65,18 +122,27 @@ int go_back(uint32_t current_time, double T, double theta[12])
     tort_cycloid(deltaT, T, 20, 50, theta, 3);
     return 0;
 }
-
-int go_mark(uint32_t current_time, double T, double theta[12])
+/*
+*站立姿态控制
+*输入：
+    current_pitch，current_roll：当前俯仰角和横滚角
+    aimp，aimr：目标俯仰角和横滚角
+    theta：驱动空间地址
+*输出：
+    theta：驱动空间
+*/
+int stand_control(double current_pitch, double current_roll, double aimp, double aimr, double theta[12])
 {
-    uint32_t deltaT;
-    static uint32_t lastT;
-    deltaT = current_time - lastT;
-    if (deltaT > T)
-    {
-        lastT = current_time;
-        deltaT = 0;
-    }
-    mark_time(deltaT, T, 10, 0, theta, 0);
+    double pitch_out, roll_out;
+    stand_PID.pek = aimp - current_pitch;
+    stand_PID.rek = aimr - current_roll;
+    stand_PID.perr_integral += stand_PID.pek;
+    stand_PID.rerr_integral += stand_PID.rek;
+    pitch_out = stand_PID.Kp * stand_PID.pek + stand_PID.Kd * (stand_PID.pek - stand_PID.pek_1) + stand_PID.Ki * stand_PID.perr_integral;
+    roll_out = stand_PID.Kp * stand_PID.rek + stand_PID.Kd * (stand_PID.rek - stand_PID.rek_1) + stand_PID.Ki * stand_PID.rerr_integral;
+    stand_PID.pek_1 = stand_PID.pek;
+    stand_PID.rek_1 = stand_PID.rek;
+    stand((pitch_out / 180.0) * PI, (roll_out / 180.0) * PI, 0, theta);
     return 0;
 }
 
@@ -90,7 +156,6 @@ void xGaitTask()
     uint32_t t;
     float euler_angles[3];
     unsigned char ps2_data[9];
-    double pitch_out, roll_out;
     while (1)
     {
         xQueueReceive(AttitudeQueue, euler_angles, 0);
@@ -118,9 +183,8 @@ void xGaitTask()
         }
         else
         {
-            pitch_out = 1.0 * (0 - euler_angles[1]);
-            roll_out = 1.0 * (0 - euler_angles[0]);
-            stand((pitch_out / 180.0) * PI, (roll_out / 180.0) * PI, (0.0 / 180.0) * PI, theta);
+            //由于陀螺仪安装的位置，俯仰角和横滚角反过来
+            stand_control(euler_angles[1], euler_angles[0], 0, 0, theta);
         }
         xQueueSend(GaitQueue, theta, portMAX_DELAY);
     }
