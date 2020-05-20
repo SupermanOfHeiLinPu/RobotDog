5d379cf1a9633446627ddab38a6ead0879f4932a
SupermanOfHeiLinPu <2827138147@qq.com>
2020-05-20 18:56:44 +0800

    添加了keil和iar printf函数重定向到串口

--- task/printf_redefine/printf_redefine.c
+++ task/printf_redefine/printf_redefine.c
@@ -1,8 +1,34 @@
 #include "usart.h"
 #include "stm32f7xx.h"
 
+//重定向
+#define PRINT_REDIRECT_UART huart5
+
+//gcc编译器
+#ifdef __GNUC__
 int _write(int fd, unsigned char *pBuffer, int size)
 {
-    HAL_UART_Transmit(&huart5, pBuffer, size, 1000);
+    HAL_UART_Transmit(&PRINT_REDIRECT_UART, pBuffer, size, 1000);
     return size;
 }
+#endif
+
+//ARM_CC编译器，及keil
+#ifdef __CC_ARM
+int fputc(int ch, FILE *f)
+{
+    /* 发送一个字节数据到串口DEBUG_USART */
+    HAL_UART_Transmit(&PRINT_REDIRECT_UART, (uint8_t *)&ch, 1, 1000);
+    return (ch);
+}
+#endif
+
+//ICC_ARM编译器，即iar
+#ifdef __ICCARM__
+int fputc(int ch, FILE *f)
+{
+    /* 发送一个字节数据到串口DEBUG_USART */
+    HAL_UART_Transmit(&PRINT_REDIRECT_UART, (uint8_t *)&ch, 1, 1000);
+    return (ch);
+}
+#endif
